bad642318692c230a519f1a0090302209873549f
victor <vdcommits@outlook.com>
2023-07-19 00:01:54 +0200

    Easy command creation

--- clippy-ts/register-commands.ts
+++ clippy-ts/register-commands.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 
-
+const BEARER_TOKEN = process.env.DISCORD_BEARER_TOKEN;
+const DISCORD_APP_ID = process.env.DISCORD_APP_ID;
 
 export function ReadCommandsFromFile() {
-    console.log('hi');
     try {
         const data = fs.readFileSync('./resources/example-command-file.json', 'utf8');
         console.log(JSON.parse(data));
@@ -12,9 +12,28 @@ export function ReadCommandsFromFile() {
     }
 }
 
+async function dropSteveCommand(jsonData: object) {
+    const response = await fetch(`https://discord.com/api/v10/applications/${DISCORD_APP_ID}/commands`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${BEARER_TOKEN}`
+        },
+        body: JSON.stringify(jsonData),
+    });
+    const discordReply = await response.json();
+    console.log(discordReply);
+}
+
 
 export function RegisterCommands() {
-
+    const jsonData = {
+        "name": "steve",
+        "type": 1,
+        "description": "steve"
+    }
+    dropSteveCommand(jsonData)
 }
 
 ReadCommandsFromFile();
+RegisterCommands();
